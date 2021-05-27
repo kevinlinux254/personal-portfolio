fb89ad72f1c3f62a6c2be18fe5072d03d15e74fa
kevin linux <kevinonyango254@gmail.com>
2021-05-27 14:11:51 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="kevin onyango" content="kevin is a passionate full-stack web developer and android app developer with lots of knowledge in flutter, php, javascript and there frameworks">
+	<title>Kevin Onyango</title>
+	<link rel="stylesheet" type="text/css" href="styles/index.css">
+	<link rel="stylesheet" type="text/css" href="styles/mediaQueries.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+	<div class="toggle" onclick="toggleMenu();" style="background: url(images/menu.png); background-size: cover; background-position: center; background-repeat: no-repeat;"></div>
+
+	<section class="banner" id="home">
+		<div class="imgSidebar">
+			<img src="images/kevin.jpg">
+		</div>		
+		<div class="contentBx">
+			<h5 class="logoText">my portfolio</h5>
+			<div>
+				<h4><span>Hello</span>, I'm</h4>
+				<h2>Kevin Onyango Obura</h2>
+				<h4>I'm a full-stack web developer</h4>
+				<p>
+					I am Kevin Onyango, a passionate full-stack web developer currently working at Masyopnet Limited. I also do android programming and graphics/logo design.
+				</p>
+				<a href="#about" class="btn">About Me</a>
+			</div>
+		</div>
+	</section>
+
+	<section class="about" id="about">
+		<div class="title white">
+			<h2>About</h2>
+			<p>
+				I am Kevin Onyango, a passionate full-stack web developer currently working at Masyopnet Limited. I also do android programming and graphics/logo design. Technology enthusiast. Artificial Intelligence, Internet of Things, Big Data and BlockChain.
+			</p>
+		</div>
+		<div class="content">
+			<div class="textBx">
+				<p>
+					Earlier, I had opted to do medicine but when I was introduced to the world of computing by my current employer, Justus Rabach- founder of masyopnet limited, and my friend, Alvin Odanga- the owner of sasaviltechnologies, I gained a lot of interest in programming. Justus Rabach introduced me to the software world and Alvin Odanga introduced me to the hardware part of computing. It is for these two great individuals who made me who I am today. I give a lot of credit to them.
+				</p>
+				<p>
+					I opted to be a full stack web developer... which involves both the front end and back end. Though I am not limited to website development, I also do graphics/logo design and a bit of android programming. In the android programming part, am still a newbie.
+				</p>
+			</div>
+			<div class="imgBx">
+				<img src="images/kevin.jpg">
+			</div>
+		</div>
+	</section>
+
+	<section class="services" id="services">
+		<div class="title">
+			<h2>Services</h2>
+			<p>
+				My services include website design and development, graphics and logo design, android application development, network installation, and configuration. I also do offer a hand in various projects when requested to.
+			</p>
+		</div>
+		<div class="content">
+			<div class="servicesBx">
+				<img src="images/web1.png">
+				<h2>Web Development</h2>
+				<p>
+					I design and develop quality and interactive websites. I use PHP, Javascript programming languages, and their frameworks (PHP-CodeIgniter and Javascript library, jQuery) to achieve this. In addition, I have mastered markup languages, HTML5, CSS3. 
+				</p>
+			</div>
+			<div class="servicesBx">
+				<img src="images/android.png">
+				<h2>Android Development</h2>
+				<p>
+					I do basic android programming using the flutter language that uses dart. I choose flutter for its simplicity and few lines of code, though am still a newbie, I am hopeful to develop quality websites soon when I have mastered android development.
+				</p>
+			</div>
+			<div class="servicesBx">
+				<img src="images/desgn.png">
+				<h2>Graphics Design</h2>
+				<p>
+					I do logo/graphic designs. I use Adobe Photoshop and adobe xd to be able to accomplish my graphics quality. My recent logo design is the masyopnet limited logo which I designed using Microsoft windows powerpoint.
+				</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="work" id="work">
+		<div class="title">
+			<h2>Recent Works</h2>
+			<p>
+				My recent works include the design and development of the masyopnet limited website and its logo. I currently have an ongoing project which I will shed more details on as soon as I have finished it.
+			</p>
+		</div>
+		<div class="content">
+			<div class="workBx">
+				<div class="imgBx">
+					<img src="images/m.jpg">
+				</div>
+				<div class="textBx">
+					<h3>Project 01 || masyopnet website</h3>
+					<p>
+						This was my greatest milestone. Developing a fully functional and website for a company, masyopnet limited its all a matter of believing you can do it..."No human is limited"...
+					</p>
+				</div>
+			</div>
+			<div class="workBx">
+				<div class="imgBx">
+					<img src="images/m.jpg">
+				</div>
+				<div class="textBx">
+					<h3>Project 02 || masyopnet logo</h3>
+					<p>
+						As the saying goes "use what you have...". I was able to design the masyopnet limited website logo using Microsoft windows PowerPoint 2019 as of that time my machine could support Adobe photoshop and adobe xd.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="contact" id="contact">
+		<div class="title">
+			<h2>Contact</h2>
+			<p>
+				You can get in touch with me on the following social media handles, my number, mail account, or developers platforms like GitHub.
+			</p>
+		</div>
+		<div class="content">
+			<div class="contactBx">
+				<h4>Contacts</h4>
+				<p>
+					<a href="tel:+254 790 938 408"><i class="fas fa-phone-square"></i></a>
+					<a href="mailto:kevinonyango254@gmail.com"><i class="far fa-envelope"></i></a>
+				</p>
+			</div>
+			<div class="contactBx">
+				<h4>Social Media</h4>
+				<p>
+					<a href="https://twitter.com/kevinlinux254"><i class="fab fa-twitter"></i></a>
+					<a href="https://linkedin.com/kevinlinux254"><i class="fab fa-linkedin-in"></i></a>
+					<a href="https://instagram.com/kevinlinux254"><i class="fab fa-instagram"></i></a>
+				</p>
+			</div>
+			<div class="contactBx">
+				<h4>Developer Arena</h4>
+				<p>
+					<a href="https://github.com/kevinlinux254"><i class="fab fa-github"></i></a>
+					<a href="https://dribble.com/kevinlinux254"><i class="fab fa-dribbble"></i></a>
+					<a href="https://codepen.io/kevinlinux254"><i class="fab fa-codepen"></i></a>
+				</p>
+			</div>
+		</div>
+		<div class="copyright">
+			<p>
+				Designed by <a href="https://kevin.masyopnet.com">Kevin Onyango</a>
+			</p>
+			<p>
+				&copy; copyright 2021. All Rights Reserved. 
+			</p>
+		</div>
+	</section>
+
+
+
+
+	<div class="sidebar">
+		<ul class="menu">
+			<li><a href="#home" onclick="toggleMenu();">Home</a></li>
+			<li><a href="#about" onclick="toggleMenu();">About</a></li>
+			<li><a href="#services" onclick="toggleMenu();">Services</a></li>
+			<li><a href="#work" onclick="toggleMenu();">Work</a></li>
+			<li><a href="#contact" onclick="toggleMenu();">Contact</a></li>
+		</ul>
+	</div>
+
+
+
+
+
+
+	<script>
+		function toggleMenu() {
+			const menuToggle = document.querySelector('.toggle');
+			const sidebar = document.querySelector('.sidebar');
+			menuToggle.classList.toggle('active');
+			sidebar.classList.toggle('active');
+
+		}
+	</script>
+
+
+
+</body>
+</html>
